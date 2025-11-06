176bb0654bd19aaaae8a4be031e905aa42ceb4be
鈴木 彦有 <>
2025-11-06 11:14:15 +0900

    [update] 'taxonkit'

--- Formula/taxonkit.rb
+++ Formula/taxonkit.rb
@@ -4,9 +4,9 @@
 class Taxonkit < Formula
   desc "A Practical and Efficient NCBI Taxonomy Toolkit, also supports creating NCBI-style taxdump files for custom taxonomies like GTDB/ICTV"
   homepage "https://bioinf.shenwei.me/taxonkit"
-  url "https://github.com/shenwei356/taxonkit/archive/refs/tags/v0.17.0.tar.gz"
-  version "0.17.0"
-  sha256 "973431541ab737d094258704ebadf238e1159764655cc72b1dcd8a601d9f9c6a"
+  url "https://github.com/shenwei356/taxonkit/archive/refs/tags/v0.20.0.tar.gz"
+  version "0.20.0"
+  sha256 "eb5a6641264f84997eaa22df7c9cad735c100b434dfd62c2560aba78164f34f6"
   license "MIT"
 
   depends_on "go" => :build
